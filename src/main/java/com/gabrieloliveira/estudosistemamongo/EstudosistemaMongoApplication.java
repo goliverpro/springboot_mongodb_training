71f8bf0ae769c51262e2f32445a3c52bf264c648
Gabriel Oliveira <gosilva.pro@gmail.com>
2020-12-28 22:24:00 -0300

    Projeto criado

---
+++ src/main/java/com/gabrieloliveira/estudosistemamongo/EstudosistemaMongoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieloliveira.estudosistemamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosistemaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosistemaMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrieloliveira/estudosistemamongo/EstudosistemaMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieloliveira.estudosistemamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosistemaMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
